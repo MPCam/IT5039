79488a0caffd6b3ae7ee4668da47b94804fecc60
Morgan Campbell <20241069@mywhitecliffe.com>
2024-10-17 10:33:14 +1300

    Activity one

--- week2/Scripts/tax.js
+++ week2/Scripts/tax.js
@@ -6,11 +6,21 @@ if ( salary > 0 ) {
         tax = .105 * salary;
     }
     else if ( salary <= 48000) {
-        tax = .105 * 14000 + .175 * (salary - 14000)
+        tax = .105 * 14000 + .175 * ( salary - 14000 );
     }
     else if ( salary <= 70000 ) {
-        tax = .105 * 14000 + .175 * (48000 -14000) + .300 * (salary - 48000)
+        tax = .105 * 14000 + .175 * ( 48000 -14000 ) + .300 * ( salary - 48000 );
     }
+    else if ( salary > 70000 ) {
+        tax = .105 * 14000 + .175 * ( 48000 - 14000 ) + .300 * ( 70000 - 48000 ) + .330 * ( salary - 70000 );
+    }
+    alert('Salary is: ' + salary + '\nTax amount is: ' + tax );
+    document.writeln('Salary is: ' + salary + "<br/>Tax amount is: " + tax);
+}
+else {
+    alert('Salary is: ' + salary + '\nTax not computed, invalid value entered for salary!' );
+    document.writeln('Salary is: ' + salary + '<br/>Tax not computed, invalid value entered for salary!' )
+
 }
 
 alert('Your tax is: ' + tax)
