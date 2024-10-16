59aaa444e954bcd94b2d46033b38e48cc6456c75
Morgan Campbell <20241069@mywhitecliffe.com>
2024-10-16 15:24:06 +1300

    week2

---
+++ week2/Scripts/practicescript.js
@@ -0,0 +1,51 @@
+/*
+var date = new Date();
+document.body.innerHTML = '<h1>Today is: ' + date + '</h1>';
+*/
+
+/*
+let a = 5;
+let b = 4;
+let sum = a+b;
+
+let humanSum = 'The value of a: ' + a + '. The value of b: ' + b + '. The sum of a and b: ' + sum + '.';
+console.log(humanSum)
+*/
+
+/*
+if ( some condition ) {
+    Do something.  //When condition is true
+} else {
+    Do something else.  //When condition is false
+}
+*/
+
+/*
+let c = 5;
+let d = 4;
+var theNumbersMatch;
+
+if ( c==d ) {
+    theNumbersMatch = true;
+} else {
+    theNumbersMatch = false;
+}
+
+console.log('The numbers match: ' + theNumbersMatch);
+*/
+
+/*
+//If a equals b AND c equals d:
+if ( a == b && c == d ) {}
+
+//If a equals b OR c equals d:
+if ( a == b || c == d ) {}
+
+//If a equals b XOR c equals d (not both):
+if (( a == b || c == d )) && (( a == b ) != ( c == d)) {}
+*/
+
+/*
+//Ternary Operator
+a == b ? console.log('Match') : console.log('No match');
+*/
